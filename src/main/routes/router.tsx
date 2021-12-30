79b13ae04f43d31f0576812d62e05ae7a7f1aecb
jeseias <domingosjeseias@gmail.com>
2021-12-30 09:04:14 +0100

    feat: signup page structure

--- src/main/routes/router.tsx
+++ src/main/routes/router.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import {  Route, Routes } from 'react-router-dom'
-import SignIn from 'presentation/pages/sign-in/sign-in'
+import {SignIn, SignUp} from 'presentation/pages'
 import { PageRoutes } from 'main/constants/page-routes'
 
 const Router: React.FC = () => {
   return (
     <Routes>
       <Route path={PageRoutes.SignIn} element={<SignIn />}/>
+      <Route path={PageRoutes.SignUp} element={<SignUp />}/>
     </Routes>
   )
 }
---
+++ src/presentation/pages/sign-up/sign-up.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Box, Flex } from "@chakra-ui/react"
+import { LinkText, LogoSVG, Title, CustomButton, CustomInput } from 'presentation/components'
+import { PageRoutes } from 'main/constants/page-routes'
+
+const SignUp: React.FC = () => {
+  return (
+    <Box pt={["22.4rem"]}>
+      <Box maxWidth="37.5rem" width="100%" m="0 auto" p="0 2.7rem 0 2.9rem">
+        <LogoSVG width="20.524rem" height="4.3rem" />
+        <Flex p="8.528rem 0 4rem 0" justifyContent="space-between" alignItems="baseline">
+          <Title>Sign Up</Title>
+          <LinkText to={PageRoutes.SignIn} bold tiny>Already have an account</LinkText> 
+        </Flex>
+        <Box>
+          <form>
+            <Box mb="2.9rem">
+              <CustomInput label="Define a username" />
+            </Box>
+            <Box mb="2.9rem">
+              <CustomInput label="Email (optional)" />
+            </Box>
+            <CustomInput label="Set your password" />
+          </form>
+        </Box>
+        <Flex justifyContent="flex-end" mt="1.2rem">
+          <LinkText to={PageRoutes.ForgotPassword} tiny>Forgot Password?</LinkText> 
+        </Flex>
+        <Flex justifyContent="center" mt="4rem">
+          <CustomButton>Sign In</CustomButton>
+        </Flex>
+      </Box>
+    </Box>
+  )
+}
+
+export default SignUp
