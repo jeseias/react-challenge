e47972278fad881981d8d7ef9d456dd54589a6a4
jeseias <domingosjeseias@gmail.com>
2022-01-02 21:47:40 +0100

    test: ensure accepts clicks

--- src/presentation/components/custom-button/custom-button.test.tsx
+++ src/presentation/components/custom-button/custom-button.test.tsx
@@ -1,12 +1,20 @@
-import { render, screen } from '@testing-library/react';
+import { ButtonProps } from '@chakra-ui/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react'
 import CustomButton from './custom-button'
 
-const makeSut = () => render(<CustomButton>Custom Button</CustomButton>)
+const makeSut = (props?: ButtonProps) => render(<CustomButton {...props}>Custom Button</CustomButton>)
 
 describe(CustomButton.name, () => {
   it('Should render children', () => {
     makeSut()
     expect(screen.getByText('Custom Button')).toBeInTheDocument()
+  })
+
+  it('Should accept clicks', () => {
+    const click = jest.fn()
+    makeSut({ onClick: click })
+    fireEvent.click(screen.getByRole('button', { name: 'Custom Button' }))
+    expect(click).toHaveBeenCalledTimes(1)
   })
 });
