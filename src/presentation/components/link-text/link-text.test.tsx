12bcb3f43306c0fd125877780c14174eae42e81c
jeseias <domingosjeseias@gmail.com>
2022-01-02 22:09:16 +0100

    test: ensure LinkText Component renders text as expected

---
+++ src/presentation/components/link-text/link-text.test.tsx
@@ -0,0 +1,20 @@
+import {  screen } from '@testing-library/react'
+import { __render } from 'presentation/modules/test-utils'
+import React from 'react'
+import LinkText from './link-text'
+
+const makeSut = (props?: any) => __render(() =>
+  <LinkText {...props}>
+    create note
+  </LinkText>
+)
+
+describe(LinkText.name, () => {
+  it('Should as expected', () => {
+    makeSut({ to: 'notes/create' })
+    const text = screen.getByText('create note')
+    expect(text).toBeInTheDocument()
+    expect(text).toHaveStyle('font-size: 1.4rem')
+    expect(text).not.toHaveStyle('font-weight: 600')
+  })
+})
