eb05bfb9f8b23b508adc54f61c7ef8d5b28ad9f9
jeseias <domingosjeseias@gmail.com>
2022-01-02 22:48:34 +0100

    test: ensure CreateJournal Page renders as expected and calls saveJournal.save with correct values

---
+++ src/presentation/pages/create-journal/create-journal.test.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { fireEvent, render, screen } from '@testing-library/react'
+import CreateJournal from './create-journal'
+import userEvent from '@testing-library/user-event'
+import { SaveJournal } from 'domain/usecases/save-journal'
+import { mockSaveJournalModel } from 'domain/__mocks__/mock-journal'
+
+class SaveJournalSpy implements SaveJournal {
+  journal = mockSaveJournalModel()
+  params: SaveJournal.Params
+  callsCount = 0
+
+  async save(params: SaveJournal.Params): Promise<SaveJournal.Model> {
+    this.params = params
+    this.callsCount++
+    return this.journal
+  }
+}
+
+type SutTypes = {
+  saveJournalSpy: SaveJournalSpy
+}
+
+const makeSut = (): SutTypes => {
+  const saveJournalSpy = new SaveJournalSpy()
+  render(<CreateJournal saveJournal={saveJournalSpy} />)
+
+  return {
+    saveJournalSpy
+  }
+}
+
+describe('CreateJournal Page', () => {
+  it('Should render as expected', () => {
+    makeSut()
+    expect(screen.getByLabelText('company logo')).toBeInTheDocument()
+    expect(screen.getByPlaceholderText('journal title')).toBeInTheDocument()
+    expect(screen.getByText('Save journal')).toBeInTheDocument()
+  })
+
+  it('Should call saveJournal.save with correct values', () => {
+    const { saveJournalSpy } = makeSut()
+    userEvent.type(screen.getByPlaceholderText('journal title'), 'HTML_')
+    fireEvent.click(screen.getByText('Save journal'))
+    expect(saveJournalSpy.params).toEqual({
+      title: 'HTML_',
+      type: 'public'
+    })
+  })
+});
